6ef0feffffb4d75c92eb1449667f59be6dd7ef9f
Madison Barnett <Madison@Madisons-MacBook-Pro.local>
2018-02-26 09:58:42 -0500

    fixing up bootstrap

--- hesh/src/index.js
+++ hesh/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'jquery';
+// the next part is so that bootstrap can find jquery by attaching jquery to global object
+window.jQuery = window.$ = global.jquery;
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
