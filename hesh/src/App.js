9258320ea304de74a81822e1289131f029d24a9f
Madison Barnett <Madison@Madisons-MacBook-Pro.local>
2018-02-26 10:38:12 -0500

    working on getting bootstrap panel to display

--- hesh/src/App.js
+++ hesh/src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
-       Hello World
+        <div className="panel panel default">
+          <div className="panel-body">
+            Hello World
+       </div>
+        </div>
       </div>
     );
   }
--- hesh/src/index.js
+++ hesh/src/index.js
@@ -4,10 +4,11 @@ import App from './App';
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import 'bootstrap';
 import registerServiceWorker from './registerServiceWorker';
 // the next part is so that bootstrap can find jquery by attaching jquery to global object
 global.jQuery = require ('jquery');
-require('bootstrap');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
