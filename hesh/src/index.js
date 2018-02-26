6a071aadd394eace2b2726ee6000d42a0761241e
Madison Barnett <Madison@Madisons-MacBook-Pro.local>
2018-02-26 10:20:04 -0500

    holy shit guys i worked through that apparently had to install popperjs seperately

--- hesh/src/index.js
+++ hesh/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'jquery';
-// the next part is so that bootstrap can find jquery by attaching jquery to global object
-window.jQuery = window.$ = global.jquery;
-import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+// the next part is so that bootstrap can find jquery by attaching jquery to global object
+global.jQuery = require ('jquery');
+require('bootstrap');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
